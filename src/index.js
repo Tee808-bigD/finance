86273bd7dc168159ba899aa04773723ac1c75a48
Tee808-bigD <thandomzobe9@gmail.com>
2026-06-13 06:04:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,12 +20,18 @@ const PORT = process.env.PORT || 3978;
 
 // ==================== MIDDLEWARE ====================
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'x-api-key'],
-  credentials: false
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
+  res.header('Access-Control-Allow-Credentials', 'false');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(cors());
 
 app.use(express.json());
 
